5db03264e7789d5a6f2cff9056ef291d1fbb93bd
Bruno Bernard <contact.brunobernard@gmail.com>
2024-03-13 12:07:18 +0400

    feat: add dev tools

---
+++ config/app_dev.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'local_providers' => [
+        \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        \App\Providers\TelescopeServiceProvider::class,
+    ],
+];
